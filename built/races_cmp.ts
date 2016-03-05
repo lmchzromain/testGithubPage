a622d39894224eb0fc4ff012749ed15d391c13d3
Romain <lemichez.romain@outlook.com>
2016-03-05 12:27:39 +0100

    Revert "test angular2"
    This reverts commit 57aae2c01c18cdf0396ab674a6e7e04cd08f8a73.

--- built/races_cmp.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from 'angular2/core';
-
-@Component({
-  selector: 'races-cmp',
-  template: `<h2>Moi je viens d'un autre composant bitch :)</h2>`
-})
-export class RacesCmp {
-
-}
